7086ace7740be82a3a75c9c9539021fa26de1464
Cecily Savage <csavage1304@tuskegee.edu>
2020-10-09 21:24:18 -0500

    Testing teradata

--- test_teradata.py
+++ test_teradata.py
@@ -1,7 +1,7 @@
 # # -*- coding: utf-8 -*-
 from teradataml import *
 
-connection_name = create_context(host = "", username="be_tuskegee", password = "be_tuskegee")
+connection_name = create_context(host = "http://tdchackathon1.eastus.cloudapp.azure.com/", username="be_tuskegee", password = "be_tuskegee")
 
 df = DataFrame("BAG_SCAN_DETAILS")
 
