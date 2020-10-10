81c4c8bc845694a3bfd59e536a78ae638502da23
Cecily Savage <csavage1304@tuskegee.edu>
2020-10-09 21:32:58 -0500

    Testing teradata

--- test_teradata.py
+++ test_teradata.py
@@ -3,7 +3,7 @@ from teradataml import *
 
 connection_name = create_context(host = "http://tdchackathon1.eastus.cloudapp.azure.com/", username="be_tuskegee", password = "be_tuskegee")
 
-df = DataFrame("BAG_SCAN_DETAILS")
+df = DataFrame.from_table("BAG_SCAN_DETAILS")
 
 for i in df
 	print (i)
